7ee2bc8394a982e0210f35e382a3a0e102c99a08
sahunaren <sahunarendra24@gmail.com>
2026-08-18 00:39:12 +0530

    Update export_service.dart

--- lib/export_service.dart
+++ lib/export_service.dart
@@ -1,34 +1,3 @@
-import 'dart:io';
-import 'package:ffmpeg_kit_flutter_video/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_video/return_code.dart';
-import 'package:video_editor/video_editor.dart';
-
-class ExportService {
-  static Future<File?> runExportVideo(
-    VideoEditorController controller, {
-    void Function(double progress)? onProgress,
-  }) async {
-    // 1. टाइमलाइन से स्टार्ट और एंड टाइम निकालना
-    final start = controller.startTrim.inMilliseconds / 1000;
-    final duration = (controller.endTrim - controller.startTrim).inMilliseconds / 1000;
-    final inputPath = controller.file.path;
-    
-    // 2. आउटपुट फ़ाइल का पाथ तैयार करना
-    final outputPath = inputPath.replaceAll('.mp4', '_10x_export.mp4');
-
-    // 3. हमारी 18-स्टेप्स वाली टेस्टेड 10x रेंडरिंग कमांड
-    final String command = 
-        "-y -ss $start -t $duration -i \"$inputPath\" "
-        "-c:v libx264 -preset ultrafast -crf 22 "
-        "-c:a aac -b:a 128k \"$outputPath\"";
-
-    final session = await FFmpegKit.execute(command);
-    final returnCode = await session.getReturnCode();
-
-    if (ReturnCode.isSuccess(returnCode)) {
-      return File(outputPath);
-    } else {
-      return null;
-    }
-  }
-}
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_session.dart';
